853cb5ee58e2fcb386c379e95581431da03ac7b9
Daniel Perez-Rodriguez <112608773+Wemisiu@users.noreply.github.com>
2025-02-02 03:01:37 +0000

    Assignment 3

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,37 @@
+public class BankAccount {
 
+
+    public static void main (String[] args) {
+
+        float balance_account = 2175.37f;
+
+        balance_account -= 302.50f;
+
+        balance_account += 50.03f;
+
+        balance_account /= 2;
+
+        balance_account += 20;
+
+        balance_account -= 1;
+
+        balance_account *= 2;
+
+        balance_account += 1;
+
+        System.out.println();
+
+        
+        System.out.printf("%.2f", balance_account);
+
+
+
+        
+
+
+
+    }
+    
+    
+    
+}
